f992018d27352dac8950bdd1b910046db7d31e36
nergalyang <224063001@qq.com>
2017-01-12 21:56:23 +0800

    done with canvas

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -17516,22 +17516,101 @@
 	  el: '#app',
 	  template: template,
 	  initialize : function () {
-	  	var that = this;
-	  	this.model = new Model();
-	  	this.model.fetch({
-	  		success : function () {
-	  			that.render();
-	  		}
-	  	});
+	  	
 	  },
-	  ui : {
-	  	clickMe : '#clickme'
-	  },
-	  events : {
-	  	'click @ui.clickMe' :'changeModel'
-	  },
-	  changeModel : function () {
-	  	this.model.set({'name':'Akito',age:123});
+	  onRender : function () {
+	   
+	    window.onload = function(){
+	    //canvas init
+	    var canvas = document.getElementById("canvas");
+	    var ctx = canvas.getContext("2d");
+	    
+	    //canvas dimensions
+	    var W = window.innerWidth;
+	    var H = window.innerHeight;
+	    canvas.width = W;
+	    canvas.height = H;
+	    
+	    //snowflake particles
+	    var mp = 75; //max particles
+	    var particles = [];
+	    for(var i = 0; i < mp; i++)
+	    {
+	      particles.push({
+	        x: Math.random()*W, //x-coordinate
+	        y: Math.random()*H, //y-coordinate
+	        r: Math.random()*4+1, //radius
+	        d: Math.random()*mp //density
+	      });
+	    }
+	    
+	    //Lets draw the flakes
+	    function draw()
+	    {
+	      ctx.clearRect(0, 0, W, H);
+	      ctx.fillStyle = "rgba(255, 255, 255, 0.6)";
+	      ctx.beginPath();
+
+	      for(var i = 0; i < mp; i++)
+	      {
+	        var p = particles[i];
+	        // ctx.moveTo(p.x, p.y);
+	        // ctx.arc(p.x, p.y, p.r, 0, Math.PI*2, true);
+	      var img = new Image();
+	      img.src = '/images/xuehua.jpg';
+	      ctx.drawImage(img,p.x,p.y,p.r,p.r);
+	      }
+	      //ctx.fill();
+	      update();
+	    }
+	    
+	    //Function to move the snowflakes
+	    //angle will be an ongoing incremental flag. Sin and Cos functions will be applied to it to create vertical and horizontal movements of the flakes
+	    var angle = 0;
+	    function update()
+	    {
+	      angle += 0.01;
+	      for(var i = 0; i < mp; i++)
+	      {
+	        var p = particles[i];
+	        //Updating X and Y coordinates
+	        //We will add 1 to the cos function to prevent negative values which will lead flakes to move upwards
+	        //Every particle has its own density which can be used to make the downward movement different for each flake
+	        //Lets make it more random by adding in the radius
+	        p.y += Math.cos(angle+p.d) + 1 + p.r/2;
+	        p.x += Math.sin(angle) * 2;
+	        
+	        //Sending flakes back from the top when it exits
+	        //Lets make it a bit more organic and let flakes enter from the left and right also.
+	        if(p.x > W+5 || p.x < -5 || p.y > H)
+	        {
+	          if(i%3 > 0) //66.67% of the flakes
+	          {
+	            particles[i] = {x: Math.random()*W, y: -10, r: p.r, d: p.d};
+	          }
+	          else
+	          {
+	            //If the flake is exitting from the right
+	            if(Math.sin(angle) > 0)
+	            {
+	              //Enter from the left
+	              particles[i] = {x: -5, y: Math.random()*H, r: p.r, d: p.d};
+	            }
+	            else
+	            {
+	              //Enter from the right
+	              particles[i] = {x: W+5, y: Math.random()*H, r: p.r, d: p.d};
+	            }
+	          }
+	        }
+	      }
+	    }
+	    
+	    //animation loop
+	    setInterval(draw, 33);
+	  };
+
+
 	  }
 	});
 
@@ -17546,7 +17625,7 @@
 	var Handlebars = __webpack_require__(11);
 	function __default(obj) { return obj && (obj.__esModule ? obj["default"] : obj); }
 	module.exports = (Handlebars["default"] || Handlebars).template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
-	    return "<div class=\"navContainer\">\n	<header>\n		<ul>\n			<li>笨飞的网站\n			<li>Ben&Phil's Website\n		</ul>\n	</header>\n	<div class=\"container\">\n		<div class=\"row\">\n			<ul>\n				<li><a href=\"#\">大数据</a></li>\n				<li><a href=\"#\">机器学习</a></li>\n				<li><a href=\"#\">Javascript</a></li>\n				<li><a href=\"#\">关于我们</a></li>\n			</ul>\n		</div>\n	</div>\n	<footer>\n		<a>Ben_Phil</a>\n	</footer>\n</div>";
+	    return "<canvas id=\"canvas\" style=\"width:100%;height:100%;\"></canvas>\n<div class=\"navContainer\">\n	<header>\n		<ul>\n			<li>笨飞的网站\n			<li>Ben&Phil's Website\n		</ul>\n	</header>\n	<div class=\"container\">\n		<div class=\"row\">\n			<ul>\n				<li><a href=\"#\">大数据</a></li>\n				<li><a href=\"#\">机器学习</a></li>\n				<li><a href=\"#\">Javascript</a></li>\n				<li><a href=\"#\">关于我们</a></li>\n			</ul>\n		</div>\n	</div>\n	<footer>\n		<a>Ben_Phil</a>\n	</footer>\n</div>";
 	},"useData":true});
 
 /***/ },
